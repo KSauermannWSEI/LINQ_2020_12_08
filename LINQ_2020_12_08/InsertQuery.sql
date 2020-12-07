2fbd767a9447ade685c07aa3a2c080c79120aa78
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2020-12-07 23:07:46 +0100

    SQL query

---
+++ LINQ_2020_12_08/InsertQuery.sql
@@ -0,0 +1,9 @@
+﻿insert into [Sales].[OrderLines]
+([OrderID],[StockItemID],[Description],[PackageTypeID], [Quantity], [UnitPrice], [TaxRate], [PickedQuantity], [PickingCompletedWhen], [LastEditedBy], [LastEditedWhen]
+)
+select 
+[OrderID],[StockItemID],[Description],[PackageTypeID], [Quantity], [UnitPrice], [TaxRate], [PickedQuantity], [PickingCompletedWhen], [LastEditedBy], [LastEditedWhen]
+from [Sales].[OrderLines]
+
+
+select COUNT(*) from [Sales].[OrderLines]
